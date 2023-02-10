80146909d867aea58c870621701e15ac05a219b4
Aayush Mohan Sinha <aayushmohansinha@gmail.com>
2023-02-10 18:25:15 +0530

    capitalise question4

--- question4.sql
+++ question4.sql
@@ -1,20 +1,20 @@
-create table detail(
-candidate_id int not null primary key,
-email varchar(100)
+CREATE TABLE detail(
+candidate_id INT NOT NULL PRIMARY KEY,
+email VARCHAR(100)
 );
 
-insert into detail values
+INSERT INTO detail VALUES
 (45, "abc@gmail.com"),
 (23, "def@yahoo.com"),
 (34, "abc@gmail.com"),
 (21, "bcf@gmail.com"),
 (94, "def@yahoo.com");
 
-select * from detail;
+SELECT * FROM detail;
 
-select * from detail 
-where candidate_id in ( 
-select min(candidate_id) from detail group by email 
+SELECT * FROM detail 
+WHERE candidate_id IN ( 
+SELECT MIN(candidate_id) FROM detail GROUP BY email 
 ) 
-order by candidate_id desc;
+ORDER BY candidate_id DESC;
 
