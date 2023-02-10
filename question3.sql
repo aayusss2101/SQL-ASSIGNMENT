57556f65c3edec997ae25be670e0bfe82b0bc01f
Aayush Mohan Sinha <aayushmohansinha@gmail.com>
2023-02-10 18:22:55 +0530

    capitalise question3

--- question3.sql
+++ question3.sql
@@ -1,22 +1,22 @@
-create table test (
-candidate_id int,
-marks int,
-primary key(candidate_id)
+CREATE TABLE test (
+candidate_id INT,
+marks INT,
+PRIMARY KEY(candidate_id)
 );
 
-insert into test values
+INSERT INTO test VALUES
 (1,98),
 (2,78),
 (3,87),
 (4,98),
 (5,78);
 
-select * from test;
+SELECT * FROM test;
 
-select distinct marks "Marks", 
-rank() over (order by marks desc) as "Rank", 
-group_concat(candidate_id) as "Candidate_ID" 
-from test 
-group by marks 
-order by marks desc;
+SELECT DISTINCT marks "Marks", 
+RANK() OVER (ORDER BY marks DESC) AS "Rank", 
+GROUP_CONCAT(candidate_id) AS "Candidate_ID" 
+FROM test 
+GROUP BY marks 
+ORDER BY marks DESC;
 
